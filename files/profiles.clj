90467e2a2e32da6d243907f1df447a97b8097137
Dorab Patel <dorab@matchcraft.com>
2018-08-29 19:16:04 -0700

    Update lein profiles.

--- files/profiles.clj
+++ files/profiles.clj
@@ -1,8 +1,11 @@
 {:repl {:dependencies [[org.clojure/tools.nrepl "RELEASE"]]}
- :user {:plugins [[lein-ancient "RELEASE"]    ;look for outdated dependency versions
-                  [venantius/yagni "RELEASE"] ;look for dead code
-                  [jonase/eastwood "RELEASE"] ;clojure lint
-                  [lein-pprint "RELEASE"]       ;pprint the project.clj
+ :user {:plugins [[lein-ancient "RELEASE"]      ; look for outdated dependency versions
+                  [venantius/yagni "RELEASE"]   ; look for dead code
+                  [jonase/eastwood "RELEASE"]   ; clojure lint
+                  [lein-kibit "RELEASE"]        ; code suggestions
+                  [lein-bikeshed "RELEASE"]     ; check spacing and lengths
+                  [criterium "0.4.4"]           ; benchmarking
+                  [lein-pprint "RELEASE"]       ; pprint the project.clj
                   ]
         :dependencies [
                        [org.clojure/clojure "1.9.0"]

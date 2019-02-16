fb62ebcd4a6ca3d5af3f1acc252d90ba3a67fb61
Dorab Patel <dorab@matchcraft.com>
2019-02-16 14:52:05 -0800

    Added speculative in clojure.

--- files/init.el
+++ files/init.el
@@ -65,6 +65,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-clojure-cli-global-options "-A:test:speculative:deps")
  '(ido-mode t nil (ido))
  '(inhibit-startup-screen t)
  '(line-move-visual nil)

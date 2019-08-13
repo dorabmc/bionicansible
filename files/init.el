c22ab98543255dc84ee35e002f717e7a039b8247
Dorab Patel <dorab@matchcraft.com>
2019-08-12 19:32:01 -0700

    Updated deps and cider to allow for devrepl.

--- files/init.el
+++ files/init.el
@@ -116,7 +116,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cider-clojure-cli-global-options "-A:test:speculative:deps")
+ '(cider-clojure-cli-global-options "-A:test:speculative:deps:devrepl")
  '(ido-mode t nil (ido))
  '(inhibit-startup-screen t)
  '(line-move-visual nil)
